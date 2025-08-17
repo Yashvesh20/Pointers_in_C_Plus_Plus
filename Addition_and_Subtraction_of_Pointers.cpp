3507e44a57a63394fab8c01c52f8f5728bcb10e1
Yashvesh20 <yashvesh20@gmail.com>
2025-08-17 13:28:50 +0530

    Create Addition_and_Subtraction_of_Pointers.cpp

---
+++ Addition_and_Subtraction_of_Pointers.cpp
@@ -0,0 +1,22 @@
+Yashvesh Singh 24070123138
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=22;
+    int b=20;
+    int*a1;
+    a1=&a;
+    int*b1;
+    b1=&b 
+    int sum,diff;
+    sum=*a1+*b1;
+    diff=*a1-*b1;
+    cout<<"Sum is: "<<sum<<endl;
+    cout<<"Difference is: "<<diff<<endl;
+}
+Output:
+Sum is: 44
+Difference is: 2
+
+  
